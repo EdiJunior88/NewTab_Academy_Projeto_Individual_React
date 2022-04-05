8bf024217a7fd0bb4047a5762733ff2d86d12176
EdiJunior88 <er.moura88@yahoo.com.br>
2022-04-05 18:46:49 -0300

    Início do projeto

---
+++ app/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { createRoot } from "react-dom/client";
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
