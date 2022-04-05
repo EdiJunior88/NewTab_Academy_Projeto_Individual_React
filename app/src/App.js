493ba5bf0a9ec253261e25971827bf426d42aa4c
EdiJunior88 <er.moura88@yahoo.com.br>
2022-04-05 19:24:41 -0300

    Update no arquivo App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div>
-      <h1>teste</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>teste</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
